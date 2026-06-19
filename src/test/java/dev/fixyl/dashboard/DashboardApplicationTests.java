3e3399e73a2ebcc2ad82fff654e467c88905faa8
Fixyl <me@fixyl.dev>
2026-06-19 19:37:19 +0200

    Generate spring boot template

---
+++ src/test/java/dev/fixyl/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fixyl.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
