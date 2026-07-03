0fb7b994c5edddcaf280c36b2cb1b687cc77f6d3
Fixyl <me@fixyl.dev>
2026-07-03 21:09:43 +0200

    Remove dynamic data substitution for templates

--- src/main/java/dev/fixyl/dashboard/controller/TemplateController.java
+++ src/main/java/dev/fixyl/dashboard/controller/TemplateController.java
@@ -1,38 +1,14 @@
 package dev.fixyl.dashboard.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import dev.fixyl.dashboard.service.ProcessorService;
-import dev.fixyl.dashboard.service.SystemService;
 
 @Controller
 public class TemplateController {
 
-    private final SystemService systemService;
-    private final ProcessorService processorService;
-
-    public TemplateController(SystemService systemService, ProcessorService processorService) {
-        this.systemService = systemService;
-        this.processorService = processorService;
-    }
-
     @GetMapping
-    public String index(Model model) {
-        systemService.getSystem().ifPresent(system ->
-            model.addAttribute("system", system)
-        );
-
-        processorService.getProcessor().ifPresent(processor ->
-            model.addAttribute("processor", processor)
-        );
-
-        processorService.getUpdate().ifPresent(update ->
-            model.addAttribute("processorUpdate", update)
-        );
-
-        return "index";
+    public String getMain() {
+        return "main";
     }
 
 }
